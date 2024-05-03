708658ebe4a2be5fcaf4a1b88d90293c21b9d417
Kullka <loinguyen07042002@gmail.com>
2024-05-04 01:04:19 +0700

    quantization in activation

--- lstm_accelerator.srcs/sources_1/new/sigmoid_appro.sv
+++ lstm_accelerator.srcs/sources_1/new/sigmoid_appro.sv
@@ -33,11 +33,14 @@ module sigmoid_appro #( parameter OUT_BITWIDTH = 8,
 //            COEF2   = 32'h00_80_00_00,  // 0.5
 //            COEF3   = 32'h00_A0_00_00,  // 0.625
 //            COEF4   = 32'h00_D8_00_00,  // 0.84375
-            COEF0   = 32'h3e200000,
-            COEF1   = 32'h3ec00000,
-            COEF2   = 32'h3f000000,
-            COEF3   = 32'h3f200000,
-            COEF4   = 32'h3f580000,
+//            COEF0   = 32'h00000000,
+            COEF1   = 32'h00000DD9,     // 0.15625
+            COEF2   = 32'h00002523,     // 0.375
+            COEF3   = 32'h00002C4F,     // 0.5
+            COEF4   = 32'h00003762,     // 0.625
+            COEF5   = 32'h00004AC5,     // 0.8375
+            COEF6   = 32'h0000589D,     // 1
+            
             
             // IEEE
 //            RANGE1  = 32'h85_00_00_00,      // -5
@@ -46,42 +49,42 @@ module sigmoid_appro #( parameter OUT_BITWIDTH = 8,
 //            RANGE4  = 32'h01_00_00_00,      // 1
 //            RANGE5  = 32'h02_60_00_00,      // 2.375
 //            RANGE6  = 32'h05_00_00_00;      // 5
-            RANGE1  = 32'hc0a00000,
-            RANGE2  = 32'hc0180000,
-            RANGE3  = 32'hbf800000,
-            RANGE4  = 32'h3f800000,
-            RANGE5  = 32'h40180000,
-            RANGE6  = 32'h40a00000;
+            RANGE1  = 32'hFFFE44EF,             // -5
+            RANGE2  = 32'hFFFF2D8B,             // -2.375
+            RANGE3  = 32'hFFFFA763,             // -1
+            RANGE4  = 32'h0000589D,             // 1
+            RANGE5  = 32'h0000D275,             // 2.375
+            RANGE6  = 32'h0001BB11;             // 5
         
         logic   [31:0]  temp;
             
         always @(*) begin
-            if (data_in >= RANGE1) begin    // <= -5
+            if (data_in >= 32'h80000000 && data_in < RANGE1) begin    // <= -5
                 data_out    <= 0;
             end
-            else if (data_in >= RANGE2 && data_in < RANGE1) begin   // (-5, -2.375]
+            else if (data_in >= RANGE1 && data_in < RANGE2) begin   // (-5, -2.375]
                 temp = data_in >>> 5;
-                data_out = temp[31:24] + COEF0[31:24];
+                data_out = temp + COEF1;
             end
-            else if (data_in >= RANGE3 && data_in < RANGE2) begin // (-2.375, -1]
+            else if (data_in >= RANGE2 && data_in < RANGE3) begin // (-2.375, -1]
                 temp = data_in >>> 3;
-                data_out = temp[31:24] + COEF1[31:24];
+                data_out = temp + COEF2;
             end
-            else if (data_in >= 32'h80_00_00_00 && data_in < RANGE3) begin // (-1, 0]
+            else if (data_in >= RANGE3 && data_in <= 32'hFFFFFFFF) begin // (-1, 0]
                 temp = data_in >>> 2;
-                data_out = temp[31:24] + COEF2[31:24];
+                data_out = temp + COEF3;
             end
             else if (data_in >= 32'h00_00_00_00 && data_in < RANGE4) begin // (0, 1]
                 temp = data_in >>> 2;
-                data_out = temp[31:24] + COEF2[31:24];
+                data_out = temp + COEF3;
             end
             else if (data_in >= RANGE4 && data_in < RANGE5) begin
                 temp = data_in >>> 3;
-                data_out = temp[31:24] + COEF3[31:24];
+                data_out = temp + COEF4;
             end
             else if (data_in >= RANGE5 && data_in < RANGE6) begin
                 temp = data_in >>> 5;
-                data_out = temp[31:24] + COEF4[31:24];
+                data_out = temp + COEF5;
             end
             else if (data_in >= RANGE6 && data_in <= 32'h7f_ff_ff_ff) begin
                 data_out = 1;
--- lstm_accelerator.srcs/sources_1/new/tanh_appr.sv
+++ lstm_accelerator.srcs/sources_1/new/tanh_appr.sv
@@ -35,12 +35,14 @@ module tanh_appr #( parameter OUT_BITWIDTH = 8,
 //            COEF4   = 32'h00_7b_00_00,  // 0.48046875
 //            COEF5   = 32'h00_e8_00_00,  // 0.90625
             
-            COEF0   = 32'hbf680000,
-            COEF1   = 32'hbef60000,
-            COEF2   = 32'hbd500000,
-            COEF3   = 32'h3d500000,
-            COEF4   = 32'h3ef60000,
-            COEF5   = 32'h3f680000,
+            COEF0   = 32'hFFFFA4B9, // -1
+            COEF1   = 32'hFFFFAD48, // -0.90625
+            COEF2   = 32'hFFFFD425, // -0.48046875
+            COEF3   = 32'hFFFFFB5E, // -0.05078125
+            COEF4   = 32'h000004A2, // 0.05078125
+            COEF5   = 32'h00002BDB, // 0.48046875
+            COEF6   = 32'h000052B8, // 0.90625
+            COEF7   = 32'h00005B47, // 1
      // (8, 24)
 //            RANGE1  = 32'h83_00_00_00,      // -3
 //            RANGE2  = 32'h81_E8_F5_C2,      // -1.91
@@ -51,49 +53,49 @@ module tanh_appr #( parameter OUT_BITWIDTH = 8,
 //            RANGE7  = 32'h01_E8_F5_C2,      // 1.91
 //            RANGE8  = 32'h03_00_00_00;      // 3
 
-            RANGE1  = 32'hc0400000,     // -3
-            RANGE2  = 32'hbff47ae1,     // -1.91
-            RANGE3  = 32'hbf5c28f6,     // -0.86
-            RANGE4  = 32'hbe4ccccd,     // -0.2
-            RANGE5  = 32'h3e4ccccd,     // 0.2
-            RANGE6  = 32'h3f5c28f6,     // 0.86
-            RANGE7  = 32'h3ff47ae1,     // 1.91
-            RANGE8  = 32'h40400000;     // 3
+            RANGE1  = 32'hFFFEEE2B,     // -3
+            RANGE2  = 32'hFFFF51A9,     // -1.91
+            RANGE3  = 32'hFFFFB181,     // -0.86
+            RANGE4  = 32'hFFFFEDBF,     // -0.2
+            RANGE5  = 32'h00001241,     // 0.2
+            RANGE6  = 32'h00004E7F,     // 0.86
+            RANGE7  = 32'h0000AE57,     // 1.91
+            RANGE8  = 32'h000111D5;     // 3
             
      logic  [31:0]  temp;
      always @(*) begin
-        if (data_in >= RANGE1) begin
+        if (data_in >= 32'h80000000 && data_in < RANGE1) begin
             data_out    = -1;
         end
-        else if (data_in >= RANGE2 && data_in < RANGE1) begin
+        else if (data_in >= RANGE1 && data_in < RANGE2) begin
             temp        = data_in >>> 5;
-            data_out    = {1'b1, {temp[30:24] + COEF0[30:24]} };
+            data_out    = temp + COEF1;
         end
-        else if (data_in >= RANGE3 && data_in < RANGE2) begin
+        else if (data_in >= RANGE2 && data_in < RANGE3) begin
             temp        = data_in >>> 2;
-            data_out    = {1'b1, {temp[30:24] + COEF1[30:24]} };
+            data_out    = temp + COEF2;
         end
-        else if (data_in >= RANGE4 && data_in < RANGE3) begin
-            temp        = data_in + (data_in >>> 2);
-            data_out    = {1'b1, {temp[30:24] + COEF2[30:24]} };
+        else if (data_in >= RANGE3 && data_in < RANGE4) begin
+            temp        = data_in - (data_in >>> 2);
+            data_out    = temp + COEF3;
         end
-        else if (data_in >= 32'h80_00_00_00 && data_in < RANGE4) begin
-            data_out    = data_in[31:24];
+        else if (data_in >= RANGE4 && data_in <= 32'hFFFFFFFF) begin
+            data_out    = data_in[7:0];
         end
         else if (data_in >= 32'h00_00_00_00 && data_in < RANGE5) begin
-            data_out    = data_in[31:24];
+            data_out    = data_in[7:0];
         end
         else if (data_in >= RANGE5 && data_in < RANGE6) begin
             temp        = data_in - (data_in >>> 2);
-            data_out    = {1'b0, {temp[30:24] + COEF3[30:24]} };
+            data_out    = temp + COEF4;
         end
         else if (data_in >= RANGE6 && data_in < RANGE7) begin
             temp        = data_in >>> 2;
-            data_out    = {1'b0, {temp[30:24] + COEF4[30:24]} };
+            data_out    = temp + COEF5;
         end
         else if (data_in >= RANGE7 && data_in < RANGE8) begin
             temp        = data_in >>> 5;
-            data_out    = {1'b1, {temp[30:24] + COEF5[30:24]} };
+            data_out    = temp + COEF6;
         end
         else if (data_in >= RANGE8 && data_in < 32'h7f_ff_ff_ff) begin
             data_out    = 1;
